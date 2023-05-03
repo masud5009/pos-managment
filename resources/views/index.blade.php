92b2e6452375544f5c7b859bc68314ba575f953c
Masud Rana <ranaahmed269205@gmail.com>
2023-05-03 12:02:09 +0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,389 @@
+@extends('layout.main')
+@section('content')
+
+@if(session()->has('not_permitted'))
+  <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ session()->get('not_permitted') }}</div>
+@endif
+@if(session()->has('message'))
+  <div class="alert alert-success alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ session()->get('message') }}</div>
+@endif
+      @php
+        if($general_setting->theme == 'default.css'){
+          $color = '#733686';
+          $color_rgba = 'rgba(115, 54, 134, 0.8)';
+        }
+        elseif($general_setting->theme == 'green.css'){
+            $color = '#2ecc71';
+            $color_rgba = 'rgba(46, 204, 113, 0.8)';
+        }
+        elseif($general_setting->theme == 'blue.css'){
+            $color = '#3498db';
+            $color_rgba = 'rgba(52, 152, 219, 0.8)';
+        }
+        elseif($general_setting->theme == 'dark.css'){
+            $color = '#34495e';
+            $color_rgba = 'rgba(52, 73, 94, 0.8)';
+        }
+      @endphp
+      <div class="row">
+        <div class="container-fluid">
+          <div class="col-md-12">
+            <div class="brand-text float-left mt-4">
+                <h3>{{trans('file.welcome')}} <span>{{Auth::user()->name}}</span> </h3>
+            </div>
+            @if(in_array("revenue_profit_summary", $all_permission))
+            <div class="filter-toggle btn-group">
+              <button class="btn btn-secondary date-btn" data-start_date="{{date('Y-m-d')}}" data-end_date="{{date('Y-m-d')}}">{{trans('file.Today')}}</button>
+              <button class="btn btn-secondary date-btn" data-start_date="{{date('Y-m-d', strtotime(' -7 day'))}}" data-end_date="{{date('Y-m-d')}}">{{trans('file.Last 7 Days')}}</button>
+              <button class="btn btn-secondary date-btn active" data-start_date="{{date('Y').'-'.date('m').'-'.'01'}}" data-end_date="{{date('Y-m-d')}}">{{trans('file.This Month')}}</button>
+              <button class="btn btn-secondary date-btn" data-start_date="{{date('Y').'-01'.'-01'}}" data-end_date="{{date('Y').'-12'.'-31'}}">{{trans('file.This Year')}}</button>
+            </div>
+            @endif
+          </div>
+        </div>
+      </div>
+      <!-- Counts Section -->
+      <section class="dashboard-counts">
+
+    
+  <div class="container">
+    <div class="row">
+        <div class="col-md-4 col-xl-3">
+            <div class="card bg-c-blue order-card">
+                <div class="card-block">
+                    <h6 class="m-b-20">{{ trans('file.revenue') }}</h6>
+                    <h2 class="text-right"><i class="fa fa-cart-plus f-left"></i><span>{{number_format((float)$revenue, 2, '.', '')}}</span></h2>
+                    <!--<p class="m-b-0">Completed Orders<span class="f-right">351</span></p>-->
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-4 col-xl-3">
+            <div class="card bg-c-green order-card">
+                <div class="card-block">
+                    <h6 class="m-b-20">{{trans('file.profit')}}</h6>
+                    <h2 class="text-right"><i class="fa fa-rocket f-left"></i><span>{{number_format((float)$profit, 2, '.', '')}}</span></h2>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-4 col-xl-3">
+            <div class="card bg-c-yellow order-card">
+                <div class="card-block">
+                    <h6 class="m-b-20">{{trans('file.Purchase Return')}}</h6>
+                    <h2 class="text-right"><i class="fa fa-refresh f-left"></i><span>{{number_format((float)$purchase_return, 2, '.', '')}}</span></h2>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-4 col-xl-3">
+            <div class="card bg-c-pink order-card">
+                <div class="card-block">
+                    <h6 class="m-b-20">{{trans('file.Sale Return')}}</h6>
+                    <h2 class="text-right"><i class="fa fa-credit-card f-left"></i><span>{{number_format((float)$return, 2, '.', '')}}</span></h2>
+                </div>
+            </div>
+        </div>
+	</div>
+</div>
+
+<style type="text/css">
+.body{
+    margin-top:20px;
+    background:#FAFAFA;
+}
+.order-card {
+    color: #fff;
+}
+
+.bg-c-blue {
+    background: linear-gradient(45deg,#4099ff,#73b4ff);
+}
+
+.bg-c-green {
+    background: linear-gradient(45deg,#2ed8b6,#59e0c5);
+}
+
+.bg-c-yellow {
+    background: linear-gradient(45deg,#FFB64D,#ffcb80);
+}
+
+.bg-c-pink {
+    background: linear-gradient(45deg,#FF5370,#ff869a);
+}
+
+
+.card {
+    border-radius: 5px;
+    -webkit-box-shadow: 0 1px 2.94px 0.06px rgba(4,26,55,0.16);
+    box-shadow: 0 1px 2.94px 0.06px rgba(4,26,55,0.16);
+    border: none;
+    margin-bottom: 30px;
+    -webkit-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+}
+
+.card .card-block {
+    padding: 25px;
+}
+
+.order-card i {
+    font-size: 26px;
+}
+
+.f-left {
+    float: left;
+}
+
+.f-right {
+    float: right;
+}
+
+</style>
+
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-7">
+              <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                  <h4>{{trans('file.Recent Transaction')}}</h4>
+                  <div class="right-column">
+                    <div class="badge badge-primary">{{trans('file.latest')}} 5</div>
+                  </div>
+                </div>
+                <ul class="nav nav-tabs" role="tablist">
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#sale-latest" role="tab" data-toggle="tab">{{trans('file.Sale')}}</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#purchase-latest" role="tab" data-toggle="tab">{{trans('file.Purchase')}}</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#quotation-latest" role="tab" data-toggle="tab">{{trans('file.Quotation')}}</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#payment-latest" role="tab" data-toggle="tab">{{trans('file.Payment')}}</a>
+                  </li>
+                </ul>
+
+                <div class="tab-content">
+                  <div role="tabpanel" class="tab-pane fade show active" id="sale-latest">
+                      <div class="table-responsive">
+                        <table class="table">
+                          <thead>
+                            <tr>
+                              <th>{{trans('file.date')}}</th>
+                              <th>{{trans('file.reference')}}</th>
+                              <th>{{trans('file.customer')}}</th>
+                              <th>{{trans('file.status')}}</th>
+                              <th>{{trans('file.grand total')}}</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            @foreach($recent_sale as $sale)
+                            <tr>
+                              <td>{{ date($general_setting->date_format, strtotime($sale->created_at->toDateString())) }}</td>
+                              <td>{{$sale->reference_no}}</td>
+                              <td>{{$sale->customer->name}}</td>
+                              @if($sale->sale_status == 1)
+                              <td><div class="badge badge-success">{{trans('file.Completed')}}</div></td>
+                              @elseif($sale->sale_status == 2)
+                              <td><div class="badge badge-danger">{{trans('file.Pending')}}</div></td>
+                              @else
+                              <td><div class="badge badge-warning">{{trans('file.Draft')}}</div></td>
+                              @endif
+                              <td>{{$sale->grand_total}}</td>
+                            </tr>
+                            @endforeach
+                          </tbody>
+                        </table>
+                      </div>
+                  </div>
+                  <div role="tabpanel" class="tab-pane fade" id="purchase-latest">
+                      <div class="table-responsive">
+                        <table class="table">
+                          <thead>
+                            <tr>
+                              <th>{{trans('file.date')}}</th>
+                              <th>{{trans('file.reference')}}</th>
+                              <th>{{trans('file.Supplier')}}</th>
+                              <th>{{trans('file.status')}}</th>
+                              <th>{{trans('file.grand total')}}</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            @foreach($recent_purchase as $purchase)
+                            <tr>
+                              <td>{{date($general_setting->date_format, strtotime($purchase->created_at->toDateString())) }}</td>
+                              <td>{{$purchase->reference_no}}</td>
+                              @if($purchase->supplier)
+                                <td>{{$purchase->supplier->name}}</td>
+                              @else
+                                <td>N/A</td>
+                              @endif
+                              @if($purchase->status == 1)
+                              <td><div class="badge badge-success">Recieved</div></td>
+                              @elseif($purchase->status == 2)
+                              <td><div class="badge badge-success">Partial</div></td>
+                              @elseif($purchase->status == 3)
+                              <td><div class="badge badge-danger">Pending</div></td>
+                              @else
+                              <td><div class="badge badge-danger">Ordered</div></td>
+                              @endif
+                              <td>{{$purchase->grand_total}}</td>
+                            </tr>
+                            @endforeach
+                          </tbody>
+                        </table>
+                      </div>
+                  </div>
+                  <div role="tabpanel" class="tab-pane fade" id="quotation-latest">
+                      <div class="table-responsive">
+                        <table class="table">
+                          <thead>
+                            <tr>
+                              <th>{{trans('file.date')}}</th>
+                              <th>{{trans('file.reference')}}</th>
+                              <th>{{trans('file.customer')}}</th>
+                              <th>{{trans('file.status')}}</th>
+                              <th>{{trans('file.grand total')}}</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            @foreach($recent_quotation as $quotation)
+                            <tr>
+                              <td>{{date($general_setting->date_format, strtotime($quotation->created_at->toDateString())) }}</td>
+                              <td>{{$quotation->reference_no}}</td>
+                              <td>{{$quotation->customer->name}}</td>
+                              @if($quotation->quotation_status == 1)
+                              <td><div class="badge badge-danger">Pending</div></td>
+                              @else
+                              <td><div class="badge badge-success">Sent</div></td>
+                              @endif
+                              <td>{{$quotation->grand_total}}</td>
+                            </tr>
+                            @endforeach
+                          </tbody>
+                        </table>
+                      </div>
+                  </div>
+                  <div role="tabpanel" class="tab-pane fade" id="payment-latest">
+                      <div class="table-responsive">
+                        <table class="table">
+                          <thead>
+                            <tr>
+                              <th>{{trans('file.date')}}</th>
+                              <th>{{trans('file.reference')}}</th>
+                              <th>{{trans('file.Amount')}}</th>
+                              <th>{{trans('file.Paid By')}}</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            @foreach($recent_payment as $payment)
+                            <tr>
+                              <td>{{date($general_setting->date_format, strtotime($payment->created_at->toDateString())) }}</td>
+                              <td>{{$payment->payment_reference}}</td>
+                              <td>{{$payment->amount}}</td>
+                              <td>{{$payment->paying_method}}</td>
+                            </tr>
+                            @endforeach
+                          </tbody>
+                        </table>
+                      </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-5">
+              <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                  <h4>{{trans('file.Best Seller').' '.date('F')}}</h4>
+                  <div class="right-column">
+                    <div class="badge badge-primary">{{trans('file.top')}} 5</div>
+                  </div>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>SL No</th>
+                          <th>{{trans('file.Product Details')}}</th>
+                          <th>{{trans('file.qty')}}</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach($best_selling_qty as $key=>$sale)
+                        <?php $images = explode(",", $sale->product_images)?>
+                        <tr>
+                          <td>{{$key + 1}}</td>
+                          <td><img src="{{url('public/images/product', $images[0])}}" height="25" width="30"> {{$sale->product_name}} [{{$sale->product_code}}]</td>
+                          <td>{{$sale->sold_qty}}</td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+            </div>
+            <div class="col-md-6">
+             
+
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+@endsection
+
+@push('scripts')
+<script type="text/javascript">
+    // Show and hide color-switcher
+    $(".color-switcher .switcher-button").on('click', function() {
+        $(".color-switcher").toggleClass("show-color-switcher", "hide-color-switcher", 300);
+    });
+
+    // Color Skins
+    $('a.color').on('click', function() {
+        /*var title = $(this).attr('title');
+        $('#style-colors').attr('href', 'css/skin-' + title + '.css');
+        return false;*/
+        $.get('setting/general_setting/change-theme/' + $(this).data('color'), function(data) {
+        });
+        var style_link= $('#custom-style').attr('href').replace(/([^-]*)$/, $(this).data('color') );
+        $('#custom-style').attr('href', style_link);
+    });
+
+    $(".date-btn").on("click", function() {
+        $(".date-btn").removeClass("active");
+        $(this).addClass("active");
+        var start_date = $(this).data('start_date');
+        var end_date = $(this).data('end_date');
+        $.get('dashboard-filter/' + start_date + '/' + end_date, function(data) {
+            dashboardFilter(data);
+        });
+    });
+
+    function dashboardFilter(data){
+        $('.revenue-data').hide();
+        $('.revenue-data').html(parseFloat(data[0]).toFixed(2));
+        $('.revenue-data').show(500);
+
+        $('.return-data').hide();
+        $('.return-data').html(parseFloat(data[1]).toFixed(2));
+        $('.return-data').show(500);
+
+        $('.profit-data').hide();
+        $('.profit-data').html(parseFloat(data[2]).toFixed(2));
+        $('.profit-data').show(500);
+
+        $('.purchase_return-data').hide();
+        $('.purchase_return-data').html(parseFloat(data[3]).toFixed(2));
+        $('.purchase_return-data').show(500);
+    }
+</script>
+@endpush
